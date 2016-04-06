ceb396b1cfe63809c7d4ab207adababfcc7fcda4
shihuajian <shihuajian@youmi.net>
2016-04-06 16:49:53 +0800

    update MainActivity

--- app/src/main/java/com/jkarthus/joe/demo/MainActivity.java
+++ app/src/main/java/com/jkarthus/joe/demo/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 /**
- * 主页Activity
+ * 主页界面
  */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
